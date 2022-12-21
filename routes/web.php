729407035daa3599e20f3acb256c04996f60bc1a
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-21 11:23:36 +0700

    add some routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthorController::class, 'index']);
+Route::resource('items', ItemController::class);
+
+/* /order -> function 'order' "OrderController" untuk menampilkan view "order" */
+/* /order -> function 'createOrder' "OrderController" untuk menjalankan fungsi pembuatan pesanan item */
+Route::get('/nested', [CollectionController::class, 'nestedArray']);
+Route::get('/movies', [MovieController::class,'index'])->name('movies.index');
+Route::get('/movies/create', [MovieController::class,'create'])->name('movies.create');
+Route::post('/movies', [MovieController::class,'store'])->name('movies.store');
+Route::get('/movies/{movie}', [MovieController::class,'show'])->name('movies.show');
+Route::get('/movies/{movie}/edit', [MovieController::class,'edit'])->name('movies.edit');
+Route::patch('/movies/{movie}', [MovieController::class,'update'])->name('movies.update');
+Route::delete('/movies/{movie}', [MovieController::class,'destroy'])->name('movies.destroy');
