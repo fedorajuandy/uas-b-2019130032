168dbd3759eddab2fe644b89df28bf4df6873a6f
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-23 01:11:17 +0700

    add counts

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -1,13 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AppController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $items = DB::table('items')
+            ->select(DB::raw('COUNT(*) AS count'))
+            ->first();
+        $item_count = $items->count;
+
+        $orders = DB::table('orders')
+            ->select(DB::raw('COUNT(*) AS count'))
+            ->first();
+        $order_count = $orders->count;
+
+        return view('index', compact('item_count', 'order_count'));
     }
 }
