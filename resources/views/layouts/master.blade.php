6b0bcad603483ed3b9a95a485e93ae8fe7bae7f3
Fedora Yoshe Juandy <fedorajuandy@gmail.com>
2022-12-22 19:22:55 +0700

    idk pages

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('title', 'XYZOrder') | XYZ Order</title>
-        <link rel="shortcut icon" href="{{ asset('/img/logo.svg') }}">
+        <title>@yield('title', 'Home') | XYZ Order</title>
+        <link rel="shortcut icon" href="{{ asset('img/logo.svg') }}">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/style.css?v=').time() }}">
         <link rel="stylesheet" href="{{ asset('css/style1.css?v=').time() }}">
@@ -14,12 +14,12 @@
 
     <body>
         <nav class="navbar navbar-expand-md navbar-dark bg-primary">
-            <a class="navbar-brand" href="{{ route('index') }}">
+            <a class="navbar-brand" href="/">
                 <i class="fa fa-film fa-fw" aria-hidden="true"></i>
                 <span class="menu-collapsed">XYZ Order</span>
             </a>
             <div class="navbar-nav ml-auto">
-                <a class="nav-link {{ Route::is('order') ? 'active' : '' }}" href="{{ route('order') }}">Create order</a>
+                <a class="nav-link {{ Route::is('index') ? 'active' : '' }}" href="{{ route('index') }}">Create order</a>
             </div>
         </nav>
 
@@ -35,7 +35,7 @@
                             <span class="menu-collapsed">Items</span>
                         </div>
                     </a>
-                    <a href="{{ route('orderview') }}" class="bg-dark list-group-item list-group-item-action">
+                    <a href="{{ route('index') }}" class="bg-dark list-group-item list-group-item-action">
                         <div class="d-flex w-100 justify-content-start align-items-center">
                             <span class="fa fa-film fa-fw mr-3"></span>
                             <span class="menu-collapsed">Orders</span>
@@ -47,12 +47,6 @@
             <div class="col p-4">
                 @yield('content')
             </div>
-
-            <footer class="pb-2 pt-3 bg-cinereous">
-                <div class="container text-center">
-                    <h6 class="color-white">&copy; {{ date("Y") }} Fedora Yoshe Juandy</h6>
-                </div>
-            </footer>
         </div>
 
         <script src="{{ asset('js/app.js') }}"></script>
